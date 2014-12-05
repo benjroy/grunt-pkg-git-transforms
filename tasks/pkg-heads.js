d37ba940c45212edbd3d442fdb4c75a0b3786682
Benjamin Allen <ben@bittorrent.com>
2014-12-05 01:14:38 -0800

    return first useful thing.  with lots of ugly logs.

--- tasks/pkg-heads.js
+++ tasks/pkg-heads.js
@@ -4,18 +4,26 @@ module.exports = function (grunt) {
     var pkg = grunt.file.readJSON('package.json');
     grunt.log.ok('pkg', pkg);
 
-    grunt.registerTask('unshrink', function() {
-	    grunt.config.requires('unshrink.url');
-	    var config = grunt.config.get('unshrink');
-	    grunt.log.ok('config', config);
-        var deps = pkg.dependencies;
-        var toTrack = Object.keys(pkg.dependencies).reduce(function (memo, key) {
-            var matches = deps[key].match(config.url);
-            console.log('matches', matches);
-            if (matches) {
+    var getDeps = function () {
+        grunt.config.requires('unshrink.url');
+        var config = grunt.config.get('unshrink');
+        grunt.log.ok('config', config);
 
+        var deps = Object.keys(pkg.dependencies).reduce(function (memo, key) {
+            var matches = pkg.dependencies[key].match(config.url);
+            if (matches) {
+                memo[key] = pkg.dependencies[key];
             }
+            return memo;
         }, {});
+        console.log('deps', deps);
+
+        return deps;
+    };
+
+    grunt.registerTask('unshrink', function() {
+        var deps = getDeps();
+        console.log('getDeps', deps);
         // var modulesToTrack = 
         // var shrinkwrap = grunt.file.readJSON('npm-shrinkwrap.json');
 
